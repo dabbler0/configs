37dfd776434d3d62096bb7765a6bba4b55025099
David Anthony Bau <dab1998@gmail.com>
2017-11-03 18:24:34 -0400

    Fix init script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 # Install the packages we want
-sudo apt-get install git fish rxvt vim evince viewnior firefox openssh-client awesome awesome-extra texlive python
+sudo apt-get install git fish rxvt-unicode vim evince viewnior firefox openssh-client awesome awesome-extra texlive python
 
 cp .Xresources ~/.Xresources
 cp .vimrc ~/.vimrc
+cp .xinitrc ~/.xinitrc
 mkdir ~/.config # if doesn't already exist
-cp awesome ~/.config/awesome
-cp fish ~/.config/fish
+cp -r awesome ~/.config/awesome
+cp -r fish ~/.config/fish
 
 # That's it!
